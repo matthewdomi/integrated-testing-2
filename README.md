0af06c21be35fa266d5a5d7e57a96c523c0964c3
Matthew Domi <70774927+matthewdomi@users.noreply.github.com>
2022-09-22 07:28:20 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,19 @@
+## Integrated - testing
+
+Run the command
+
+`$yarn init -y`
+
+Install the following
+
+`$ yarn add -D jest`
+
+`$ yarn add axios`
+
+Run the command below to test
+
+`$ yarn run jest`
+
+In package.json
+
+` "test": "jest" `
---
+++ math.test.js
@@ -0,0 +1,36 @@
+const { substract, sum, multiply, divide } = require("./math");
+
+beforeAll(() => {
+    console.log("Before All Test")
+});
+
+describe('Functions', () => {
+    beforeEach(() => {
+        console.log("Before Each");
+    })
+
+
+    it("should sum two number", () => {
+        expect(sum(2, 3)).toBe(5);
+    })
+
+    it("should substract two number", () => {
+        expect(substract(5, 3)).toBe(2);
+    })
+
+    it("should multiply two number", () => {
+        expect(multiply(3, 3)).toBe(9);
+    })
+
+    it("should divide two number", () => {
+        expect(divide(12, 3)).toBe(4);
+    })
+
+    afterEach(() => {
+        console.log("After Each");
+    })
+})
+
+afterAll(()=>{
+    console.log("Run All Test");
+})
